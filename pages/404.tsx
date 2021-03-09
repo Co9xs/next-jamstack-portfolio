975af8888dfccc138e64bba585274e0102b1d4dd
Co9xs <ryo.fjsm0203@icloud.com>
2021-03-09 23:41:55 +0900

    Fixed 404Page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,6 +1,6 @@
 import Twemoji from 'react-twemoji';
 import { Meta } from '../components/common/Meta';
-import { PageBase, ContentSection, ContentSectionInner, SectionTitle } from '../styles/utils/common';
+import { PageBase, ContentSection, ContentSectionInner, SectionTitle, PlainText } from '../styles/utils/common';
 
 export default function Page404() {
   return (
@@ -9,9 +9,10 @@ export default function Page404() {
         title={'Page Not Found'}
         description={'ページが見つかりませんでした'}
       />
-      <ContentSection style={{ background: '#F1F5F9' }}>
+      <ContentSection style={{ background: '#F1F5F9', minHeight: '100%'}}>
         <ContentSectionInner>
-          <SectionTitle><Twemoji tag="span">🙇</Twemoji>ページが見つかりませんでした。</SectionTitle>
+          <SectionTitle><Twemoji tag="span">🙇</Twemoji>404 - Not Found</SectionTitle>
+          <PlainText>お探しのページは見つかりませんでした</PlainText>
         </ContentSectionInner>
       </ContentSection>
     </PageBase>
