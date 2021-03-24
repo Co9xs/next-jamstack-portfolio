c3918d824719845651ab8b2a075a81042dae647b
Co9xs <ryo.fjsm0203@icloud.com>
2021-03-24 15:00:05 +0900

    Fixed import statement

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,5 @@
 import Document, { Html, Head, Main, NextScript, DocumentContext } from "next/document";
-import {ServerStyleSheet} from 'styled-components';
+import { ServerStyleSheet } from 'styled-components';
 
 type Props = {};
 
