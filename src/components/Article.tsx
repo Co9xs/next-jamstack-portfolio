bfb067a467495cec7176d27e8e0ac3a591843ca4
Co9xs <ryo.fjsm0203@icloud.com>
2021-04-30 09:06:48 +0900

    ArticleCardのデザインを変更

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -1,6 +1,8 @@
 import styled from 'styled-components'
 import { Article } from '@/types';
 import { convertDateToString } from '@/utils';
+import React from 'react';
+import { ClockIcon, PersonIcon } from './icons';
 
 type Props = {
   article: Article
@@ -8,29 +10,43 @@ type Props = {
 
 export const ArticleCard: React.VFC<Props> = (props) => {
   const { article } = props
+  const image = `https://og-image-co9xs.vercel.app/${article.title}.png`
   const publishedAt = convertDateToString(new Date(article.publishedAt));
+  const tags = [{ name: '', id: 1}, {name: `TypeScript`, id: 2}, {name: 'React', id: 3}]
   return (
     <ArticleBase>
-      <ArticleHeader>
+      <ArticleImage style={{backgroundImage: `url(${image})`}}>
+      </ArticleImage>
+      <ArticleData>
         <ArticleTitle>{ article.title }</ArticleTitle>
-      </ArticleHeader>
-      <ArticleFooter>
-        <ArticleDate>{ publishedAt }</ArticleDate>
-        {article.category && 
-        <ArticleCategory>#{ article.category.name }</ArticleCategory>}
-      </ArticleFooter>
+        <ArticleCategory>{article.category.name}</ArticleCategory>
+        <ArticleTags>
+          <ArticleTag>#JavaScript</ArticleTag>
+          <ArticleTag>#TypeScript</ArticleTag>
+          <ArticleTag>#React</ArticleTag>
+        </ArticleTags>
+        <ArticleMetaData>
+          <ArticleDate>
+            <ClockIcon />
+            <ArticleDateText>
+              {publishedAt}
+            </ArticleDateText>
+          </ArticleDate>
+          <ArticleReadTime>30 min read</ArticleReadTime>
+          <ArticleAuthor>
+            <PersonIcon/>
+            <ArticleAuthorText>しま</ArticleAuthorText>
+          </ArticleAuthor>
+        </ArticleMetaData>
+      </ArticleData>
     </ArticleBase>
   )
 }
 
 const ArticleBase = styled.article`
-  padding: 0.75rem;
-  border: 2px solid #EFEFEF;
-  border-radius: 5px;
-  padding: 1.5rem;
-  margin-bottom: 1.5rem;
-  background: #FFF;
+  padding: 16px 0;
   cursor: pointer;
+  display: flex;
 
   //DarkMode
   color: ${({ theme }) => theme.white};
@@ -38,22 +54,71 @@ const ArticleBase = styled.article`
   border-color: ${({ theme }) => theme.smoke};
 `
 
-const ArticleHeader = styled.div`
-  display: flex;
+const ArticleImage = styled.div`
+  width: 350px;
+  min-width: 250px;
+  height: 175px;
+  background-color: #EEE;
+  border-radius: 3px;
+  margin-right: 40px;
+  background-size: cover;
+  background-position: center;
+`
+
+const ArticleData = styled.div`
+  max-width: 420px;
 `
 const ArticleTitle = styled.h3`
   margin: 0;
   padding: 0;
+  font-size: 20px;
+  margin-bottom: 10px;
 `
-const ArticleFooter = styled.div`
-`
+
 const ArticleCategory = styled.span`
   color: #60A5FA;
-  margin-right: .5rem;
+  padding: 4px 8px;
+  border: 2px solid #60A5FA;
+  border-radius: 3px;
+  display: inline-block;
+  margin-bottom: 10px;
 `
-const ArticleDate = styled.span`
-  color: #6B7280;
-  margin-right: 1rem;
 
+const ArticleTags = styled.div`
+  color: #616269;
+  margin-bottom: 10px;
+`
+
+const ArticleTag = styled.span`
+  margin-right: 8px;
+`
+
+const ArticleMetaData = styled.div`
+  color: #616269;
+  display: flex;
+  align-items: center;
+`
+
+const ArticleDate = styled.span`
+  margin-right: 16px;
+  display: flex;
+  align-items: center;
   color: ${({ theme }) => theme.smoke};
+`
+
+const ArticleDateText = styled.span`
+  margin-left: 8px;
+`
+
+const ArticleReadTime = styled.div`
+  margin-right: 16px;
+`
+
+const ArticleAuthor = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+const ArticleAuthorText = styled.span`
+  margin-left: 8px;
 `
