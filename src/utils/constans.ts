3cc53d496c9a7ce0368625771702c147fd3f583d
Co9xs <ryo.fjsm0203@icloud.com>
2021-05-05 16:54:48 +0900

    ページごとの記事数を5に変更

--- src/utils/constans.ts
+++ src/utils/constans.ts
@@ -1,6 +1,5 @@
 export const API_ENDPOINT: string = 'https://shima.microcms.io/api/v1';
-export const ARTICLES_IN_TOP: number = 3;
-export const ARTICLES_PER_PAGE: number = 3;
+export const ARTICLES_PER_PAGE: number = 5;
 export const CATEGORIES_PER_PAGE: number = 10;
 export const DEAFULT_HEADER_HEIGHT: number = 105;
 export const config = {
