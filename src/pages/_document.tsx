4f2341b72d11ffc3f32b6c6f9597598e3ffa0030
Co9xs <ryo.fjsm0203@icloud.com>
2021-05-20 19:39:04 +0900

    google fontを削除

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -34,7 +34,7 @@ export default class MyDocument extends Document<Props> {
         <Head>
           <GoogleAnalyticsScript/>
           <meta charSet="utf-8" />
-          <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@500;700&display=swap" rel="stylesheet" />
+          {/* <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@500;700&display=swap" rel="stylesheet" /> */}
         </Head>
         <body>
           <Main />
