80c7b6a9e4563948ea698951501ffe6ab33dd6b5
Co9xs <ryo.fjsm0203@icloud.com>
2021-05-20 15:19:36 +0900

    @next/bundle-analyzerを導入

--- next.config.js
+++ next.config.js
@@ -3,3 +3,8 @@ module.exports = {
     domains: ['og-image-co9xs.vercel.app', 'images.microcms-assets.io', 'res.cloudinary.com']
   }
 }
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+module.exports = withBundleAnalyzer({})
