1467cb956e9347508dcdf3d99383954c7839b010
Co9xs <ryo.fjsm0203@icloud.com>
2021-05-20 19:51:52 +0900

    google analytics用のタグを削除

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -32,7 +32,7 @@ export default class MyDocument extends Document<Props> {
     return (
       <Html lang="ja">
         <Head>
-          <GoogleAnalyticsScript/>
+          {/* <GoogleAnalyticsScript/> */}
           <meta charSet="utf-8" />
           {/* <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@500;700&display=swap" rel="stylesheet" /> */}
         </Head>
