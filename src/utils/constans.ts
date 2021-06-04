8aae2cddefa4a31a6f0d718ada3d4b1099e46086
Co9xs <ryo.fjsm0203@icloud.com>
2021-06-04 14:15:59 +0900

    fix constant types

--- src/utils/constans.ts
+++ src/utils/constans.ts
@@ -1,8 +1,8 @@
-export const API_ENDPOINT: string = 'https://shima.microcms.io/api/v1';
-export const ZENN_RSS: string = "https://zenn.dev/co9xs/feed";
-export const ARTICLES_PER_PAGE: number = 6;
-export const CATEGORIES_PER_PAGE: number = 10;
-export const DEAFULT_HEADER_HEIGHT: number = 103;
+export const API_ENDPOINT = 'https://shima.microcms.io/api/v1';
+export const ZENN_RSS = "https://zenn.dev/co9xs/feed";
+export const ARTICLES_PER_PAGE = 6;
+export const CATEGORIES_PER_PAGE = 10;
+export const DEAFULT_HEADER_HEIGHT = 103;
 export const config = {
   headers: { 'X-API-KEY': process.env.API_KEY },
 };
