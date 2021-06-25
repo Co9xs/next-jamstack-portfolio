e02f39b2690fdba9a4af19af6eba679df4679fa5
Co9xs <ryo.fjsm0203@icloud.com>
2021-06-25 11:41:12 +0900

    remove unused component

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,15 +1,6 @@
 // 以下はAPI経由で取得してくるデータ以外の型定義
 // APIからのレスポンスは./src/apis配下にinterfaceで定義してある
 
-export type Work = {
-  title: string,
-  date: string,
-  url: string,
-  githubUrl: string,
-  languages: string[],
-  description: string
-}
-
 export type ExternalPostItem = {
   title: string
   contentSnippet: string
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,4 @@
 export * from './constans';
-export * from './works';
 export * from './skills';
 export * from './links';
 export * from './commonFunctions';
